33c70e1f750181c01d32447872f23fede269e4b8
BloxdioCannoli <coding.cannoli@gmail.com>
2026-05-11 01:18:56 -0400

    Update world code.js

--- world code.js
+++ world code.js
@@ -15,6 +15,8 @@ let pNum = 0;
 
 let startWorldTickAt = 20;
 
+let mobs = [];
+
 let consec = 0; let wait = 0; function tick() {
     if (wait > 0) { wait--; return; } else { if (consec >= maxConsec) { consec = 0; wait = waitNum; } else { consec++; } };
 
@@ -30,8 +32,18 @@ let consec = 0; let wait = 0; function tick() {
 
                 if (mob) {
                     api.setMobAiState(mob, "walkingToPosition", { pos: brainrtoDeathPos });
+                    mobs.push(mob);
                 }
-                //log(mob);
+            }
+
+            for (let mNum in mobs) {
+                let m = mobs[mNum];
+                let [x, y, z] = api.getPosition(m);
+
+                if (z <= brainrtoDeathPos[1]) {
+                    api.despawnMob(m);
+                    mobs.splice(m, 1);
+                }
             }
         }
     } else {
