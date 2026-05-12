bec5738a0fe0fc8722e8df9ffc912cf813ec873e
BloxdioCannoli <coding.cannoli@gmail.com>
2026-05-11 20:36:29 -0400

    Refactor spawn positions and update laser settings

--- world code.js
+++ world code.js
@@ -1,4 +1,4 @@
-//update: aaaaa
+//update: a
 
 brainrotSpawnPos = [-999, -999, -1025];
 brainrotDeathPos = [-999, -997, -942.5];
@@ -30,12 +30,12 @@ basesConfig = [
     { nametagPos: [-1018, -994, -957], spawnPos: [-1021, -998, -957], borders: [[-1018, -999, -950], [-1029, -982, -963]], laserStartPos: [-1018, -999, -957], otherLasers: [[-1027, -990, -952], [-1028, -990, -952]], lockPos: [-1019, -996, -960] },
     { nametagPos: [-1018, -994, -976], spawnPos: [-1020, -998, -976], borders: [[-1018, -999, -969], [-1029, -982, -982]], laserStartPos: [-1018, -999, -976], otherLasers: [[-1027, -990, -971], [-1028, -990, -971]], lockPos: [-1019, -996, -979] },
     { nametagPos: [-1018, -994, -994], spawnPos: [-1020, -998, -995], borders: [[-1018, -999, -988], [-1029, -982, -1001]], laserStartPos: [-1018, -999, -994], otherLasers: [[-1027, -990, -989], [-1028, -990, -989]], lockPos: [-1019, -996, -997] },
-    { nametagPos: [-1018, -994, -1014], spawnPos: [-1020, -998, -1014], borders: [[-1018, -999, -1007], [-1029, -982, -1020]], laserStartPos: [-1018, -999, -1013], otherLasers: [[-1027, -990, -1008], [-1028, -990, -1008]], lockPos: [-1019, -996, -1017] },
+    { nametagPos: [-1018, -994, -1013], spawnPos: [-1020, -998, -1014], borders: [[-1018, -999, -1007], [-1029, -982, -1020]], laserStartPos: [-1018, -999, -1013], otherLasers: [[-1027, -990, -1008], [-1028, -990, -1008]], lockPos: [-1019, -996, -1016] },
 
-    { nametagPos: [-984, -994, -957], spawnPos: [-984, -998, -957], borders: [[-984, -982, -952], [-973, -999, -940]], laserStartPos: [-984, -999, -957], otherLasers: [[-993, -990, -952], [-994, -990, -952]], lockPos: [-982, -996, -960] },
-    { nametagPos: [-984, -994, -976], spawnPos: [-984, -998, -976], borders: [[-984, -982, -965], [-973, -999, -953]], laserStartPos: [-984, -999, -976], otherLasers: [[-993, -990, -971], [-994, -990, -971]], lockPos: [-982, -996, -979] },
-    { nametagPos: [-984, -994, -995], spawnPos: [-984, -998, -995], borders: [[-984, -982, -978], [-973, -999, -966]], laserStartPos: [-984, -999, -994], otherLasers: [[-993, -990, -989], [-994, -990, -989]], lockPos: [-982, -996, -997] },
-    { nametagPos: [-984, -994, -1014], spawnPos: [-984, -998, -1014], borders: [[-984, -982, -991], [-973, -999, -979]], laserStartPos: [-984, -999, -1013], otherLasers: [[-993, -990, -1008], [-994, -990, -1008]], lockPos: [-982, -996, -1017] },
+    { nametagPos: [-984, -994, -957], spawnPos: [-984, -998, -957], borders: [[-984, -982, -952], [-973, -999, -940]], laserStartPos: [-984, -999, -957], otherLasers: [[-975, -990, -1019], [-974, -990, -1019]], lockPos: [-983, -996, -960] },
+    { nametagPos: [-984, -994, -976], spawnPos: [-984, -998, -976], borders: [[-984, -982, -965], [-973, -999, -953]], laserStartPos: [-984, -999, -976], otherLasers: [[-975, -990, -1000], [-974, -990, -1000]], lockPos: [-983, -996, -979] },
+    { nametagPos: [-984, -994, -994], spawnPos: [-984, -998, -995], borders: [[-984, -982, -978], [-973, -999, -966]], laserStartPos: [-984, -999, -994], otherLasers: [[-975, -990, -982], [-974, -990, -982]], lockPos: [-983, -996, -997] },
+    { nametagPos: [-984, -994, -1013], spawnPos: [-984, -998, -1014], borders: [[-984, -982, -991], [-973, -999, -979]], laserStartPos: [-984, -999, -1013], otherLasers: [[-975, -990, -963], [-974, -990, -963]], lockPos: [-983, -996, -1016] },
 ];
 
 let consec = 0; let wait = 0; function tick() {
@@ -127,6 +127,7 @@ let consec = 0; let wait = 0; function tick() {
                             autoRotate: true,
 
                             blockName: brainrotData.blockName,
+                            hideDist: 250,
                         });
                         api.setPosition(mesh, x, y, z);
 
@@ -229,7 +230,7 @@ function onPlayerLeave(myId) {
 }
 
 function onPlayerJoin(myId) {
-    api.setItemStat(myId, "Invisible Solid", "showInCreativeInven", true);
+    api.setWalkThroughType(myId, "Pink Portal");
     api.setMaxPlayers(8, 8);
     api.setWalkThroughRect(myId, [-1000, -997, -942], [-999, -1000, -941], 0);
 
@@ -383,6 +384,7 @@ function createLaser(x, y, z, height = 5) {
     laser1 = api.attemptCreateMeshEntity("BloxdBlock", {
         size: [0.5, height, 0.5],
         blockName: "Red Concrete",
+        hideDist: 100,
     }, "laser");
     api.setTargetedPlayerSettingForEveryone(laser1, "nameTagInfo", { content: [] });
     api.setPosition(laser1, [x + 0.5, y + 1, z + 0.5]);
@@ -401,7 +403,7 @@ function createLockNotif(myId, pos) {
     lockNotif = api.attemptCreateMeshEntity("BloxdBlock", {
         size: [1, 1, 1],
         blockName: "Invisible Solid",
-        hideDist: 5,
+        hideDist: 10,
     });
     api.setOtherEntitySetting(myId, lockNotif, "nameTagInfo", {
         content: [
