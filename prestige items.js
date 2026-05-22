a2747ee5a2256525f397a6ebecabeb5d9acbe584
BloxdioCannoli <coding.cannoli@gmail.com>
2026-05-22 02:23:54 -0400

    Create prestige items.js

---
+++ prestige items.js
@@ -0,0 +1,63 @@
+api.clearInventory(myId);
+
+
+api.giveItem(myId, "White Torch", 1, {
+    customDisplayName: "Flashbang",
+    customDescription: "A player in a 5 block radius has their screen go white for a few seconds.",
+
+    customAttributes: {
+        enchantments: {},
+        enchantmentTier: "Tier 5"
+    }
+});
+
+api.giveItem(myId, "Fireball Block", 1, {
+    customDisplayName: "Swap Crystal",
+    customDescription: "Swap with a player in a 5 block radius.",
+
+    customAttributes: {
+        enchantments: {},
+        enchantmentTier: "Tier 5"
+    }
+});
+
+api.giveItem(myId, "Iceball Block", 1, {
+    customDisplayName: "Freeze Ray",
+    customDescription: "Freeze a player in a 5 block radius.",
+
+    customAttributes: {
+        enchantments: {},
+        enchantmentTier: "Tier 5"
+    }
+});
+
+api.giveItem(myId, "Stick", 1, {
+    customDisplayName: "Galaxy Bat",
+    customDescription: "Launch a player into the next galaxy.",
+
+    customAttributes: {
+        enchantments: {},
+        enchantmentTier: "Tier 5"
+    }
+});
+
+// FINISHED
+api.giveItem(myId, "Spirit Saddle", 1, {
+    customDisplayName: "67 Saddle",
+    customDescription: "Ride a six-seveeven six-seeeeeven",
+
+    customAttributes: {
+        enchantments: {},
+        enchantmentTier: "Tier 5"
+    }
+});
+
+api.giveItem(myId, "Black Concrete Slab", 1, {
+    customDisplayName: "Invisibility Hat",
+    customDescription: "Become invisible until you perform an action like stealing.",
+
+    customAttributes: {
+        enchantments: {},
+        enchantmentTier: "Tier 5"
+    }
+});
